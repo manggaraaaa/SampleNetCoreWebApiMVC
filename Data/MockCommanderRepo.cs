ebe2bfdbac14023a1cf1d351895ba142f51898d8
Muhammad Ari Anggara <muh.ari.anggara@gmail.com>
2020-05-08 13:32:53 +0700

    full course

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0,HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"},
+                new Command{Id=1,HowTo="Read a book", Line="Read book", Platform="Book & Lamp"},
+                new Command{Id=3,HowTo="Make a coffe", Line="Make coffe", Platform="Cup & Spoon"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
